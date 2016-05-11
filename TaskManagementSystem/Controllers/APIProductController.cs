7b474fafa885d267bede7634913e617c7df79259
Glee Earl Rupal <Glee Earl Rupal>
2016-05-11 17:13:21 +0800

    first initial commit

---
+++ TaskManagementSystem/Controllers/APIProductController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+
+namespace TaskManagementSystem.Controllers
+{
+    public class APIProductController : ApiController
+    {
+        private Data.TMSdbmlDataContext db = new Data.TMSdbmlDataContext();
+
+        // ===========
+        // LIST Item
+        // =========== 
+        [Route("api/product/list")]
+        public List<Models.MstProduct> Get()
+        {
+            var isLocked = true;
+
+            var product = from d in db.mstProducts
+                        select new Models.MstProduct
+                        {
+                            Id = d.Id,
+                            ProductCode = d.ProductCode,
+                            ProductDescription = d.ProductDescription,
+                            IsLocked = isLocked
+                        };
+
+            return product.ToList();
+        }
+
+        // ==============
+        // DELETE Item
+        // ==============
+        [Route("api/product/delete/{id}")]
+        public HttpResponseMessage Delete(String id)
+        {
+
+            try
+            {
+                var productId = Convert.ToInt32(id);
+                var products = from d in db.mstProducts where d.Id == productId select d;
+
+                if (products.Any())
+                {
+                    db.mstProducts.DeleteOnSubmit(products.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+
+    }
+
+    
+}
---
+++ TaskManagementSystem/Controllers/SoftwareController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskManagementSystem.Controllers
+{
+    public class SoftwareController : Controller
+    {
+        // GET: Software
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult Staff()
+        {
+            return View();
+        }
+       
+
+    }
+
+  
+
+}
